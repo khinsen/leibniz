1811b9592435e5b6f927199b891285329d660169
Konrad Hinsen <konrad.hinsen@fastmail.net>
2016-06-10 10:24:20 +0200

    Term substitution in patterns

--- leibniz/terms.rkt
+++ leibniz/terms.rkt
@@ -33,13 +33,13 @@
 ;
 (define-generics term
   [term.sort term]
-  [term.pattern? term]
+  [term.has-vars? term]
   #:fast-defaults
   ([number?
     (define term.sort number-term.sort)
-    (define (term.pattern? x) #f)])
+    (define (term.has-vars? x) #f)])
   #:fallbacks
-  [(define (term.pattern? x) #f)])
+  [(define (term.has-vars? x) #f)])
 
 (module+ test
   (check-equal? (term.sort 0) 'Zero)
@@ -47,7 +47,7 @@
   (check-equal? (term.sort -1) 'NonZeroInteger)
   (check-equal? (term.sort 1/2) 'PositiveRational)
   (check-equal? (term.sort -1/2) 'NonZeroRational)
-  (check-false (term.pattern? 1)))
+  (check-false (term.has-vars? 1)))
 
 ;
 ; Substitutions are var->term hashes describing a match.
@@ -70,6 +70,9 @@
             #f
             (hash-set s-acc var value)))))
 
+(define (substitution-value substitution var)
+  (hash-ref substitution var #f))
+
 (module+ test
   ; Substitutions are meant to be used as var->term mappings,
   ; but they are really just hash maps with special key collision
@@ -85,7 +88,15 @@
   (check-false (merge-substitutions (substitution 'A 1)
                                     (substitution 'A 2)))
   (check-false (merge-substitutions #f (substitution 'A 2)))
-  (check-false (merge-substitutions (substitution 'A 2) #f)))
+  (check-false (merge-substitutions (substitution 'A 2) #f))
+  (check-equal? (substitution-value
+                 (merge-substitutions (substitution 'A 1)
+                                      (substitution 'B 2))
+                 'B) 2)
+  (check-false (substitution-value
+                (merge-substitutions (substitution 'A 1)
+                                     (substitution 'B 2))
+                'C)))
 
 ;
 ; Matching returns a sequence of all match-generating substitutions.
@@ -102,17 +113,26 @@
 
 ;
 ; The generic interface for patterns
+; Non-pattern term data types implement trivial versions
+; of its methods, in which matching is an equality test
+; and substitution does nothing.
 ;
 (define (non-pattern-match signature term1 term2)
   (conditional-match (equal? term1 term2) empty-substitution))
 
+(define (non-pattern-substitute signature term substitution)
+  term)
+
 (define-generics pattern
   [pattern.match signature pattern term]
+  [pattern.substitute signature pattern substitution]
   #:fast-defaults
   ([number?
-    (define pattern.match non-pattern-match)])
+    (define pattern.match non-pattern-match)
+    (define pattern.substitute non-pattern-substitute)])
   #:fallbacks
-  [(define pattern.match non-pattern-match)])
+  [(define pattern.match non-pattern-match)
+   (define pattern.substitute non-pattern-substitute)])
 
 (define (all-matches signature pattern term)
   (sequence->list (pattern.match signature pattern term)))
@@ -121,7 +141,15 @@
   (define-simple-check (check-no-match signature pattern term)
     (= 0 (sequence-length (pattern.match signature pattern term))))
   (define-simple-check (check-single-match signature pattern term substitution)
-    (equal? (all-matches signature pattern term) (list substitution))))
+    (equal? (all-matches signature pattern term) (list substitution)))
+  (define-simple-check (check-self-substitution signature pattern term)
+    (equal? (pattern.substitute signature pattern
+                                (first (all-matches signature pattern term)))
+            term))
+  (define-simple-check (check-no-substitution signature pattern)
+    (equal? (pattern.substitute signature pattern
+                                (substitution (var 'a-strange-name 'Zero) 0))
+            pattern)))
 
 (module+ test
   (check-single-match a-signature 1 1 empty-substitution)
@@ -140,16 +168,16 @@
 (module+ test
   (define an-A (make-term a-signature 'an-A empty))
   (check-equal? (term.sort an-A) 'A)
-  (check-false (term.pattern? an-A))
+  (check-false (term.has-vars? an-A))
   (define a-B (make-term a-signature 'a-B empty))
   (check-equal? (term.sort a-B) 'B)
-  (check-false (term.pattern? a-B))
+  (check-false (term.has-vars? a-B))
   (define an-X (make-term a-signature 'an-X empty))
   (check-equal? (term.sort an-X) 'X)
-  (check-false (term.pattern? an-X))
+  (check-false (term.has-vars? an-X))
   (define a-Y (make-term a-signature 'a-Y empty))
   (check-equal? (term.sort a-Y) 'Y)
-  (check-false (term.pattern? a-Y))
+  (check-false (term.has-vars? a-Y))
  
   (check-equal? (term.sort (make-term a-signature 'foo empty)) 'B)
   (check-equal? (term.sort (make-term a-signature 'foo (list a-B))) 'A)
@@ -177,22 +205,29 @@
   #:methods gen:term
   [(define (term.sort t)
      (var-sort-or-kind t))
-   (define (term.pattern? t)
+   (define (term.has-vars? t)
      #t)]
   #:methods gen:pattern
   [(define (pattern.match signature pattern term)
      (conditional-match (conforms-to? (signature-sort-graph signature)
                                       (term.sort term)
                                       (var-sort-or-kind pattern))
-                        (substitution pattern term)))])
+                        (substitution pattern term)))
+   (define (pattern.substitute signature pattern substitution)
+     (define value (substitution-value substitution pattern))
+     (if value
+         value
+         pattern))])
 
 (module+ test
-  (check-true (term.pattern? (var 'foo 'A)))
+  (check-true (term.has-vars? (var 'foo 'A)))
   (check-single-match a-signature (var 'foo 'Zero) 0
                       (substitution (var 'foo 'Zero) 0))
   (check-single-match a-signature (var 'foo 'Integer) 0
                       (substitution (var 'foo 'Integer) 0))
-  (check-no-match a-signature (var 'foo 'NonZeroInteger) 0))
+  (check-no-match a-signature (var 'foo 'NonZeroInteger) 0)
+  (check-self-substitution a-signature (var 'foo 'Zero) 0)
+  (check-no-substitution a-signature (var 'bar 'Zero)))
 
 ;
 ; Operator-defined patterns
@@ -204,7 +239,7 @@
   #:methods gen:term
   [(define (term.sort t)
      (op-term-sort t))
-   (define (term.pattern? t)
+   (define (term.has-vars? t)
      #t)]
   #:methods gen:pattern
   [(define/generic generic-match pattern.match)
@@ -227,7 +262,14 @@
       [(not (equal? (length p-args) (length t-args)))
        no-match]
       [else
-       (match-args p-args t-args empty-substitution)]))])
+       (match-args p-args t-args empty-substitution)]))
+
+   (define/generic generic-substitute pattern.substitute)
+   (define (pattern.substitute signature pattern substitution)
+     (make-term signature
+                (op-term-op pattern)
+                (for/list ([arg (op-term-args pattern)])
+                  (generic-substitute signature arg substitution))))])
 
 (module+ test
   (define var-X (var 'X 'B))
@@ -235,7 +277,7 @@
 
   (define a-one-var-pattern (make-term a-signature 'foo (list var-X)))
   (check-equal? (term.sort a-one-var-pattern) 'A)
-  (check-true (term.pattern? a-one-var-pattern))
+  (check-true (term.has-vars? a-one-var-pattern))
   (check-single-match a-signature a-one-var-pattern
                       (make-term a-signature 'foo (list a-B))
                       (substitution var-X a-B))
@@ -244,7 +286,7 @@
 
   (define a-two-var-pattern (make-term a-signature 'foo (list var-Y var-X)))
   (check-equal? (term.sort a-two-var-pattern) 'A)
-  (check-true (term.pattern? a-two-var-pattern))
+  (check-true (term.has-vars? a-two-var-pattern))
   (check-single-match a-signature a-two-var-pattern
                       (make-term a-signature 'foo (list an-A a-B))
                       (merge-substitutions
@@ -259,7 +301,7 @@
   (define a-double-var-pattern (make-term a-signature 'foo (list var-X var-X)))
   (define foo0 (make-term a-signature 'foo empty))
   (check-equal? (term.sort a-double-var-pattern) 'A)
-  (check-true (term.pattern? a-double-var-pattern))
+  (check-true (term.has-vars? a-double-var-pattern))
   (check-single-match a-signature a-double-var-pattern
                       (make-term a-signature 'foo (list a-B a-B))
                       (substitution var-X a-B))
@@ -267,7 +309,11 @@
                       (make-term a-signature 'foo (list foo0 foo0))
                       (substitution var-X foo0))
   (check-no-match a-signature a-double-var-pattern
-                  (make-term a-signature 'foo (list a-B foo0))))
+                  (make-term a-signature 'foo (list a-B foo0)))
+  (check-self-substitution a-signature
+                           a-double-var-pattern
+                           (make-term a-signature 'foo (list a-B a-B)))
+  (check-no-substitution a-signature a-double-var-pattern))
 
 ;
 ; Make an operator-defined term. The result is a pattern
@@ -275,9 +321,8 @@
 ;
 (define (make-term signature op args)
   (define rank (lookup-op signature op (map term.sort args)))
-  (define pattern? (ormap term.pattern? args))
   (and rank
-       (if pattern?
+       (if (ormap term.has-vars? args)
            (op-pattern op args (cdr rank))
            (op-term op args (cdr rank)))))
 
