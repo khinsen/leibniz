df25f6ff6540b71eeca7681f572b719a1070f7ab
Konrad Hinsen <konrad.hinsen@fastmail.net>
2016-07-04 11:13:15 +0200

    More compact syntax for rewriting in contexts

---
+++ leibniz/context-syntax.rkt
@@ -0,0 +1,53 @@
+#lang racket
+
+(provide with-context R T RT
+         (rename-out [c:define-context define-context]))
+
+(require (prefix-in c: "./contexts.rkt")
+         "./term-syntax.rkt"
+         "./rewrite.rkt"
+         racket/stxparam
+         (for-syntax syntax/parse
+                     racket/stxparam))
+
+(module+ test
+  (require chk)
+  (c:define-context test-context
+    (include c:truth-context)
+    (op (not Boolean) Boolean)
+    (op foo Boolean)
+    (=> (not true) false)
+    (=> (not false) true)
+    (=> foo (not true) #:if false)
+    (=> foo (not false) #:if true)))
+
+(define-syntax-parameter R
+  (λ (stx)
+    (raise-syntax-error 'R "R keyword used outside with-context" stx)))
+
+(define-syntax-parameter RT
+  (λ (stx)
+    (raise-syntax-error 'RT "RT keyword used outside with-context" stx)))
+
+(define-syntax (with-context stx)
+  (syntax-parse stx
+    [(_ context:expr body:expr ...)
+     #'(syntax-parameterize
+           ([R (λ (stx)
+                 (syntax-parse stx
+                   [(_ term:expr)
+                    #'(reduce context term)]))]
+            [RT (λ (stx)
+                  (syntax-parse stx
+                    [(_ term)
+                     #'(reduce context (T term))]))])
+         (c:with-context context
+           body) ...)]))
+
+(module+ test
+  (with-context test-context
+    (chk
+     #:= (RT (not true))        (T false)
+     #:= (RT (not false))       (T true)
+     #:= (RT (not (not false))) (T false)
+     #:= (R (T foo))            (T true))))
