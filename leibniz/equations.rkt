ade4e30ec8cac9c9787267bc08260a9cb7f2e5e8
Konrad Hinsen <konrad.hinsen@fastmail.net>
2016-07-10 17:37:59 +0200

    Rule validation and rulelist merge

--- leibniz/equations.rkt
+++ leibniz/equations.rkt
@@ -5,10 +5,13 @@
  (contract-out
   [make-rule (signature? term? (or/c #f term?) (or/c term? procedure?)
               . -> . rule?)]
+  [valid-rule? (signature? any/c . -> . boolean?)]
   [rulelist? (any/c . -> . boolean?)]
   [empty-rulelist rulelist?]
+  [in-rules (rulelist? . -> . stream?)]
   [add-rule (rulelist? rule? . -> . rulelist?)]
-  [lookup-rules (rulelist? term? . -> . list?)]))
+  [lookup-rules (rulelist? term? . -> . list?)]
+  [merge-rulelists (signature? rulelist? rulelist? . -> . rulelist?)]))
 
 (require "./sorts.rkt"
          "./operators.rkt"
@@ -60,12 +63,22 @@
                    replacement (term.sort pattern))))
   (rule pattern condition replacement))
 
+(define (valid-rule? signature rule)
+  (and (rule? rule)
+       (valid-term? signature (rule-pattern rule))
+       (or (not (rule-condition rule))
+           (valid-term? signature (rule-condition rule)))
+       (or (procedure? (rule-replacement rule))
+           (valid-term? signature (rule-replacement rule)))))
+
 (module+ test
   (with-sig-and-vars a-signature a-varset
     (check-equal? (make-rule a-signature (T IntVar) #f (T 2))
                   (rule (T IntVar) #f (T 2)))
     (check-equal? (make-rule a-signature (T IntVar) (T true) (T 2))
                   (rule (T IntVar) (T true) (T 2)))
+    (check-true (valid-rule? a-signature (rule (T IntVar) #f (T 2))))
+    (check-true (valid-rule? a-signature (rule (T IntVar) (T true) (T 2))))
     ; Term 'bar not allowed in signature
     (check-exn exn:fail? (thunk (make-rule a-signature 'bar #f (T 2))))
     (check-exn exn:fail? (thunk (make-rule a-signature (T Avar) 'bar (T 2))))
@@ -81,6 +94,33 @@
     ; Replacement doesn't match sort of pattern
     (check-exn exn:fail?
                (thunk (make-rule a-signature (T IntVar) #f (T (foo a-B)))))))
+
+; Convert a rule to a new (larger) signature. Used for merging contexts.
+(define (in-signature rule signature)
+  (make-rule signature
+             (term.in-signature (rule-pattern rule) signature)
+             (let ([c (rule-condition rule)])
+               (if c
+                   (term.in-signature c signature)
+                   c))
+             (let ([r (rule-replacement rule)])
+               (if (procedure? r)
+                   r
+                   (term.in-signature r signature))) ))
+
+(module+ test
+  (define larger-signature
+    (~> a-signature
+        (add-op 'foo (list 'A) 'A)))
+  (with-sig-and-vars a-signature a-varset
+    (check-true
+     (valid-rule? larger-signature
+                  (in-signature (make-rule a-signature (T foo) #f (T foo))
+                                larger-signature)))
+    (check-true
+     (valid-rule? larger-signature
+                  (in-signature (make-rule a-signature (T IntVar) #f (T 2))
+                                larger-signature)))))
 
 ;
 ; Rule lists
@@ -104,17 +144,31 @@
 (define (lookup-rules rulelist term)
   (hash-ref rulelist (term.key term) empty))
 
+(define (in-rules rulelist)
+  (for*/stream ([(key rules) rulelist]
+                [rule rules])
+     rule))
+
+(define (merge-rulelists merged-signature rl1 rl2)
+  (for/fold ([rules empty-rulelist])
+            ([rule (stream-append (in-rules rl1) (in-rules rl2))])
+    (add-rule rules (in-signature rule merged-signature))))
+
 (module+ test
   (with-sig-and-vars a-signature a-varset
+    (define rule1 (make-rule a-signature (T IntVar) #f (T 2)))
+    (define rule2 (make-rule a-signature (T (foo Bvar)) #f (T Bvar)))
+    (define rule3 (make-rule a-signature (T (foo Avar Bvar)) #f (T (foo Bvar))))
     (define some-rules
       (~> empty-rulelist
-          (add-rule (make-rule a-signature
-                               (T IntVar) #f (T 2)))
-          (add-rule (make-rule a-signature
-                               (T (foo Bvar)) #f (T Bvar)))
-          (add-rule (make-rule a-signature
-                               (T (foo Avar Bvar)) #f (T (foo Bvar))))))
+          (add-rule rule1)
+          (add-rule rule2)
+          (add-rule rule3)))
     (check-equal? (hash-count some-rules) 2)
     (check-equal? (length (lookup-rules some-rules (T foo))) 2)
     (check-equal? (length (lookup-rules some-rules (T IntVar))) 1)
-    (check-true (empty? (lookup-rules some-rules (T an-A))))))
+    (check-true (empty? (lookup-rules some-rules (T an-A))))
+    (check-equal? (stream-length (in-rules some-rules)) 3)
+    (check-equal? (stream->list (in-rules some-rules)) (list rule1 rule2 rule3))
+    (check-equal? (merge-rulelists a-signature empty-rulelist some-rules) some-rules)
+    (check-equal? (merge-rulelists a-signature some-rules empty-rulelist) some-rules)))
