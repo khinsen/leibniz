6e1cb2eaa22b240be14711122be1eea2f70f4bf3
Konrad Hinsen <konrad.hinsen@fastmail.net>
2016-11-03 08:30:32 +0100

    Improved graph drawing
    The graphs have been cleaned up by removing extraneous edges.

--- leibniz/tools.rkt
+++ leibniz/tools.rkt
@@ -18,7 +18,7 @@
             ([(from to) node-character-translation])
     (string-replace s from to)))
 
-(define (sort-graph->graphviz file-path s-graph [sort-or-kind #f])
+(define (sort-graph->graphviz s-graph [sort-or-kind #f])
   (define sorts
     (cond
       [(sort-graph? s-graph) s-graph]
@@ -28,26 +28,24 @@
   (define subset (if sort-or-kind
                      (kind sorts sort-or-kind)
                      (all-sorts sorts)))
-  (with-output-to-file (expand-user-path file-path)
-    (thunk (printf "digraph sort_graph {\n")
-           (for ([sort subset])
-             (printf "~a [label=\"~a\"];\n"
-                     (symbol->node sort) (symbol->string sort)))
-           (printf "rankdir=BT;\n")
-           (for ([ss (all-subsort-relations sorts)]
-                 #:when (set-member? subset (car ss)))
-             (printf "~a->~a;\n"
-                     (symbol->node (car ss))
-                     (symbol->node (cdr ss))))
-           (when sort-or-kind
-             (printf "label=\"Sort graph of ~a\";\n"
-                     (constraint->string sorts subset))
-             (printf "labelloc=top;\n")
-             (printf "labeljust=left;\n"))
-           (printf "}\n"))
-    #:mode 'text #:exists 'truncate))
+  (printf "digraph sort_graph {\n")
+  (for ([sort subset])
+    (printf "~a [label=\"~a\"];\n"
+            (symbol->node sort) (symbol->string sort)))
+  (printf "rankdir=BT;\n")
+  (for ([ss (all-subsort-relations sorts)]
+        #:when (set-member? subset (car ss)))
+    (printf "~a->~a;\n"
+            (symbol->node (car ss))
+            (symbol->node (cdr ss))))
+  (when sort-or-kind
+    (printf "label=\"Sort graph of ~a\";\n"
+            (constraint->string sorts subset))
+    (printf "labelloc=top;\n")
+    (printf "labeljust=left;\n"))
+  (printf "}\n"))
 
-(define (op->graphviz file-path sig op-symbol arg-sorts)
+(define (op->graphviz sig op-symbol arg-sorts)
 
   (define signature
     (cond
@@ -77,24 +75,40 @@
                    (λ (ns) (set-add ns (node-label rank2)))
                    (set (node-label rank2)))))
 
-  (with-output-to-file (expand-user-path file-path)
-    (thunk (printf "digraph op_ranks {\n")
-           (printf "rankdir=BT;\n")
-           (printf "node [shape=box];\n")
-           (for ([rank ranks])
-             (printf "~a [label=\"~a\"];\n"
-                     (node-label rank)
-                     (rank->string rank)))
-           (for* ([(node1 node2-list) (subarity-graph ranks)]
-                  [node2 node2-list])
-             (printf "~a -> ~a\n" node1 node2))
-           (printf "label=\"Operator ~a for arity ~s\";\n"
-                   op-symbol
-                   arg-sorts)
-           (printf "labelloc=top;\n")
-           (printf "labeljust=left;\n")
-           (printf "}\n"))
-    #:mode 'text #:exists 'truncate))
+  (define (indirect sa-graph)
+    (for*/fold ([graph (hash)])
+               ([(from tos) sa-graph]
+                [to tos])
+      (define second (hash-ref sa-graph to (set)))
+      (hash-update graph from
+                   (λ (ns) (set-union ns second))
+                   second)))
+
+  ; Simplify a subarity graph by removing all edges that
+  ; are equivalent to existing indirect paths.
+  (define (simplified sa-graph)
+    (define i (indirect sa-graph))
+    (for/hash ([(from tos) sa-graph])
+      (values from (set-subtract tos (hash-ref i from)))))
+
+  (printf "digraph op_ranks {\n")
+  (printf "rankdir=BT;\n")
+  (printf "node [shape=box];\n")
+  (for ([rank ranks])
+    (printf "~a [label=\"~a\"];\n"
+            (node-label rank)
+            (rank->string rank)))
+
+  (for* ([(node1 node2-list) (simplified (subarity-graph ranks))]
+         [node2 node2-list])
+    (printf "~a -> ~a\n" node1 node2))
+
+  (printf "label=\"Operator ~a for arity ~s\";\n"
+          op-symbol
+          arg-sorts)
+  (printf "labelloc=top;\n")
+  (printf "labeljust=left;\n")
+  (printf "}\n"))
 
 (define filename-character-translation
   (hash "/" "SLASH"
@@ -118,7 +132,9 @@
   (define sorts (signature-sort-graph signature))
   (define base-path (expand-user-path directory-path))
   (make-directory* base-path)
-  (sort-graph->graphviz (build-path base-path "sorts.dot") sorts)
+  (with-output-to-file (build-path base-path "sorts.dot")
+    (thunk (sort-graph->graphviz sorts))
+    #:mode 'text #:exists 'truncate)
   (for ([(symbol k-arity) (ops-by-kind-arity signature)])
     (define arg-sorts (for/list ([kind k-arity])
                         (set-first (maximal-sorts sorts kind))))
@@ -129,5 +145,6 @@
                                (cons symbol arg-sorts)))
                     "-")
        ".dot"))
-    (op->graphviz (build-path base-path fname)
-                  signature symbol arg-sorts)))
+    (with-output-to-file (build-path base-path fname)
+      (thunk (op->graphviz signature symbol arg-sorts))
+      #:mode 'text #:exists 'truncate)))
